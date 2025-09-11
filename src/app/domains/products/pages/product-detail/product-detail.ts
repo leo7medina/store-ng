1bc147c65b529ca79ec5c19ff7af40d7902325eb
Leonardo Medina <leo_medina14@hotmail.com>
2025-09-10 23:19:56 -0500

    Primitivas reactivas: Uso del Linked Signal y Computed

--- src/app/domains/products/pages/product-detail/product-detail.ts
+++ src/app/domains/products/pages/product-detail/product-detail.ts
@@ -1,4 +1,11 @@
-import { Component, inject, OnInit, signal, input } from '@angular/core';
+import {
+  Component,
+  inject,
+  OnInit,
+  signal,
+  input,
+  linkedSignal,
+} from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Product } from '@shared/models/product';
 import { ProductService } from '@shared/services/product-service';
@@ -13,8 +20,28 @@ import { CartService } from '@shared/services/cart-service';
 export class ProductDetail implements OnInit {
   readonly id = input<string>();
   readonly slug = input<string>();
-  product = signal<Product | null>(null);
-  cover = signal('');
+  $product = signal<Product | null>(null);
+  /**
+   * Se puede utilizar de esta manera el linkedSignal
+  $cover = linkedSignal(() => {
+    const product = this.$product();
+    if (product && product.images.length > 0) {
+      return product.images[0];
+    }
+    return '';
+  });*/
+  /**
+   * Otra manera de utilizar el linkedSignal.
+   */
+  $cover = linkedSignal({
+    source: () => this.$product(),
+    computation: (product, previous) => {
+      if (product && product?.images?.length > 0) {
+        return product.images[0];
+      }
+      return previous?.value ?? '';
+    },
+  });
   private productService = inject(ProductService);
   private cartService = inject(CartService);
 
@@ -34,21 +61,16 @@ export class ProductDetail implements OnInit {
     product_slug?: string;
   }) {
     this.productService.getOne(params).subscribe({
-      next: (product) => {
-        this.product.set(product);
-        if (product.images.length > 0) {
-          this.cover.set(product.images[0]);
-        }
-      },
+      next: (product) => this.$product.set(product),
     });
   }
 
   changeCover(newImg: string) {
-    this.cover.set(newImg);
+    this.$cover.set(newImg);
   }
 
   addToCart() {
-    const product = this.product();
+    const product = this.$product();
     if (product) {
       this.cartService.addToCart(product);
     }
